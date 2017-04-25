d550cab7c7b73f4370cb0b0198cd2a8c364f45d8
SinigagliaSteeve <steeve.sinigaglia@gmail.com>
2017-04-25 09:57:49 +0200

    Implements GET /anecdotes and fix issue in anecdotes.json

--- app/anecdotes.js
+++ app/anecdotes.js
@@ -1,12 +1,15 @@
+const Good = require('good');
+const Joi = require('joi');
 
+const anecdotes = require('../data/anecdotes.json');
 
 module.exports = function(server) {
-    
+
     server.route({
         method: 'GET',
         path: '/anecdotes',
         handler: (request, reply) => {
-            reply('OK');
+            reply(anecdotes);
         }
     });
 

d1c6932ecd6aaf211adfad6ab6f629f34b07cfd3
SinigagliaSteeve <steeve.sinigaglia@gmail.com>
2017-04-25 10:07:46 +0200

    Implements GET /anecdotes/{id}

--- app/anecdotes.js
+++ app/anecdotes.js
@@ -3,6 +3,10 @@ const Joi = require('joi');
 
 const anecdotes = require('../data/anecdotes.json');
 
+function getAnecdote(id) {
+  return anecdotes.find(a => a.id === id);
+}
+
 module.exports = function(server) {
 
     server.route({
@@ -13,6 +17,19 @@ module.exports = function(server) {
         }
     });
 
+    server.route({
+        method: 'GET',
+        path: '/anecdotes/{anecdoteid}',
+        handler : (request, reply) => {
+          const id = encodeURIComponent(request.params.anecdoteid);
+          const anecdote = getAnecdote(id);
+          if (anecdote===undefined) {
+            reply("This anecdote was not found").code(404);
+          } else {
+            reply(anecdote);
+          }
+        }
+    })
 
 
 };
